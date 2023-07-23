b955362d0707c2c7c2d036fb1dd7e2ba40317eee
jvorndran <135787855+jvorndran@users.noreply.github.com>
2023-07-22 22:07:37 -0400

    commit

--- base/urls.py
+++ base/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='Home'),
-    path('calender/', views.calender, name='Calender'),
+    path('', views.calender, name='Calender'),
     path('<str:date>/', views.calender_detail, name='calender_detail'),
 ]
